19e03bd3fdd6fd585d750b3d03d215a3bb62caaf
juanjbsalas <jbsalas05@gmail.com>
2025-07-18 14:18:36 -0400

    implement async function

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-fetch("http://www.omdbapi.com/?apikey=77a8b526&t=Star+Wars")
-    .then(response => response.json())
-    .then(data => console.log(data.Title))
-    .catch(error => console.error(error));
+// fetch("http://www.omdbapi.com/?apikey=77a8b526&t=Star+Wars")
+//     .then(response => {
+//         if(!response.ok ){
+//             throw new Error("Could not fetch resource");
+//         }
+//         return response.json();
+//     })
+//     .then(data => console.log(data))
+//     .catch(error => console.error(error));
+
+url = "http://www.omdbapi.com/?apikey=77a8b526&"
+
+async function fetchData() {
+    try {
+        const response = await fetch("http://www.omdbapi.com/?apikey=77a8b526&t=Star+Wars")
+
+        if(!response.ok){
+            throw new Error("Could not fetch resource");
+        }
+
+        const data = await response.json();
+        console.log(data);
+    }
+    catch(error){
+        console.error(error)
+    }
+    
+}
+
+fetchData();
+
+
