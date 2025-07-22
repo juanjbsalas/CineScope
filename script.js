d8656d33d6dd5920e05a8a76a57c3e40faa6444c
juanjbsalas <jbsalas05@gmail.com>
2025-07-22 14:20:30 -0400

    remove card button

--- script.js
+++ script.js
@@ -75,11 +75,6 @@ async function fetchData() {
                                                                             </ul>
                                                                         </p>
                                                                     </div>
-                                                                    <div class="px-4 pb-4 pt-0 mt-2">
-                                                                        <button class="rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
-                                                                            Read more
-                                                                        </button>
-                                                                    </div>
                                                                 </div>` 
                                                                 + document.getElementById('movie-info1').innerHTML;
             // movTitle = data.Title
